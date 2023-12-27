ca1076322b83ccc5a513c5713ef2d178d53d742d
Rahmat Aditya Qudaedah <adityaqudaedah@gmail.com>
2023-12-27 11:25:48 +0700

    refactor : router instance

--- main.go
+++ main.go
@@ -5,14 +5,12 @@ import (
 
 	"github.com/adityaqudaedah/go_restful_api/app"
 	"github.com/adityaqudaedah/go_restful_api/controller"
-	"github.com/adityaqudaedah/go_restful_api/exception"
 	"github.com/adityaqudaedah/go_restful_api/helpers"
 	"github.com/adityaqudaedah/go_restful_api/middleware"
 	"github.com/adityaqudaedah/go_restful_api/repository"
 	"github.com/adityaqudaedah/go_restful_api/service"
 	"github.com/go-playground/validator/v10"
 	_ "github.com/go-sql-driver/mysql"
-	"github.com/julienschmidt/httprouter"
 )
 
 func main() {
@@ -22,16 +20,7 @@ func main() {
 	categoryService := service.NewCategoryService(categoryRepository,db,validate)
 	categoryController := controller.NewCategoryController(categoryService)
 
-	router := httprouter.New()
-
-	router.POST("/api/categories",categoryController.Create)
-	router.GET("/api/categories",categoryController.FindAll)
-	router.GET("/api/categories/:categoryId",categoryController.FindById)
-	router.PUT("/api/categories/:categoryId",categoryController.Update)
-	router.DELETE("/api/categories/:categoryId",categoryController.Delete)
-
-	router.PanicHandler = exception.ErrorHandler
-
+	router := app.NewRouter(categoryController)
 	server := http.Server{
 		Addr: ":8080",Handler: middleware.NewAuthMiddleWare(router),
 	}
